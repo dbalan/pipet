e30f62b207284701ae0190be6dd541dcd79de5bd
Dhananjay Balan <dhananjay@cliqz.com>
2018-02-21 18:42:32 +0100

    Implment marshalling

--- pipetdata/pipetdata.go
+++ pipetdata/pipetdata.go
@@ -2,6 +2,12 @@
 // being called out from CLI
 package pipetdata
 
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
+)
+
 // DataStore is the main structure for snippet access
 type DataStore struct {
 	documentDir string
@@ -9,24 +15,38 @@ type DataStore struct {
 
 // Metadata for snippet
 type metadata struct {
-	Title string
-	Tags  []string
+	Title string   `yaml:"title"`
+	Tags  []string `yaml:"tags,omitempty"`
 }
 
 // Snippet is the data type holding the actual snippet
 type Snippet struct {
-	meta metadata
+	Meta metadata
 	Data string
 }
 
-// MarshalMD serializes snippet data into markdown. Uses markdown with yaml
-// metadata block on top
-func (s *Snippet) MarshalMD() []byte {
-	return []byte("NA")
+// Marshal serializes snippet data into bytes. Format is
+// ---
+// yaml metadata front
+// ---
+// <text follows>
+// This is very similiar to pandoc markdown except its just arbitary text for now.
+func (s *Snippet) Marshal() ([]byte, error) {
+	template := `---
+%s---
+%s
+`
+	meta, err := yaml.Marshal(s.Meta)
+	if err != nil {
+		return []byte{}, errors.Wrap(err, "yaml rendering failed")
+	}
+
+	rendered := fmt.Sprintf(template, meta, s.Data)
+	return []byte(rendered), nil
 }
 
-// UnmarshalMD takes in markdown data with metadata blocks in yaml and populates
+// UnmarshalMD takes in note data with metadata blocks in yaml and populates
 // Snippet structure.
-func (s *Snippet) UnmarshalMD(data []byte) error {
+func (s *Snippet) Unmarshal(data []byte) error {
 	return nil
 }
