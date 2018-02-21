ffabd13f493ff8ccee69dec5c0474a6f38c1f614
Dhananjay Balan <dhananjay@cliqz.com>
2018-02-21 12:07:02 +0100

    Reinit with BSD

--- cmd/root.go
+++ cmd/root.go
@@ -43,10 +43,13 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "pipet",
-	Short: "Store and sprinkle code snippets",
-	Long: `Pipet is a small snippet manager to store, search and reuse code snippets
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
 
-It is inspired from pet and other snippet managers.`,
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
