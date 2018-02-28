1daa4d5c30abe29abe25ec2d5cd7f0a3460758a1
Dhananjay Balan <mail@dbalan.in>
2018-02-28 13:02:40 +0100

    Add echo command for piping data to shell

--- cmd/root.go
+++ cmd/root.go
@@ -93,6 +93,6 @@ func initConfig() {
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+		// fmt.Fprintf(os.Stderr, "Using config file: %s", viper.ConfigFileUsed())
 	}
 }
